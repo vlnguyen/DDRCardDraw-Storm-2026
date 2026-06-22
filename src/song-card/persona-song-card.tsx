d4dd3ffc35192c4362e551109adb18c33a39ce66
Vincent <vincentlocnguyen@gmail.com>
2026-06-22 12:55:06 -0700

    show BPM on card, remove click events on readonly card

--- src/song-card/persona-song-card.tsx
+++ src/song-card/persona-song-card.tsx
@@ -1,24 +1,17 @@
-import { Popover } from "@blueprintjs/core";
 import classNames from "classnames";
-import { type JSX, useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useConfigState } from "../state/hooks";
-import { CHART_PLACEHOLDER, DrawnChart } from "../models/Drawing";
-import { SongSearch } from "../song-search";
-import { FillPlaceholderList, ActionMenu } from "./acton-menu";
+import { CHART_PLACEHOLDER } from "../models/Drawing";
 import styles from "./persona-song-card.css";
 import protectIcon from "../assets/img/protect.svg";
 import vetoIcon from "../assets/img/veto.svg";
+import coffinBlueIcon from "../assets/img/coffin-blue.svg";
 import { getJacketUrl } from "../utils/jackets";
-import { copyTextToClipboard } from "../utils/share";
 import { useChartRandomSelected } from "../tournament-mode/highlight-random";
 import { usePlayerLabelForIndex } from "./use-player-label";
 
 import { baseChartValues } from "./variants";
-import {
-  type PlayerIdx,
-  type SongCardBaseProps as Props,
-  useIconCallbacksForChart,
-} from "./song-card";
+import { type SongCardBaseProps as Props } from "./song-card";
 
 export function PersonaSongCard(props: Props) {
   const {
@@ -28,14 +21,12 @@ export function PersonaSongCard(props: Props) {
     replacedBy,
     replacedWith,
     winner,
-    actionsEnabled,
     CenterContent,
     FooterContent,
   } = props;
   const hideVetos = useConfigState((s) => s.hideVetos);
 
-  const [wasRandomlySelected, clearRandomSelection] =
-    useChartRandomSelected(chart);
+  const [wasRandomlySelected] = useChartRandomSelected(chart);
   const rootRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (wasRandomlySelected && rootRef.current) {
@@ -46,24 +37,10 @@ export function PersonaSongCard(props: Props) {
     }
   }, [wasRandomlySelected]);
 
-  const [showingContextMenu, setContextMenuOpen] = useState(false);
-  const showMenu = () => setContextMenuOpen(true);
-  const hideMenu = () => setContextMenuOpen(false);
-
-  const [pocketPickPendingForPlayer, setPocketPickPendingForPlayer] =
-    useState<PlayerIdx | null>(null);
-
   const baseChartIsPlaceholder =
     "type" in chart && chart.type === CHART_PLACEHOLDER;
 
-  const { name, diffAbbr, jacket } = replacedWith || baseChartValues(chart);
-
-  const hasLabel = !!(
-    vetoedBy !== undefined ||
-    protectedBy !== undefined ||
-    replacedBy !== undefined
-  );
-  const hasWinner = typeof winner === "number";
+  const { jacket, bpm } = replacedWith || baseChartValues(chart);
 
   const isVetoed = vetoedBy !== undefined;
   const headerPlayer = vetoedBy ?? protectedBy ?? replacedBy;
@@ -76,87 +53,18 @@ export function PersonaSongCard(props: Props) {
     jacketBg = { backgroundImage: `url("${getJacketUrl(jacket)}")` };
   }
 
-  const iconCallbacks = useIconCallbacksForChart((chart as DrawnChart).id);
-  const handleCopy = useCallback(async () => {
-    if (!diffAbbr) {
-      return;
-    }
-    await copyTextToClipboard(
-      `${name} [${diffAbbr.toUpperCase()}]`,
-      "Copied name & difficulty",
-    );
-  }, [name, diffAbbr]);
-  const canCopy = !!name && !!diffAbbr;
-
-  let menuContent: undefined | JSX.Element;
-  if (actionsEnabled && !hasWinner) {
-    if (replacedWith === undefined && baseChartIsPlaceholder) {
-      menuContent = (
-        <FillPlaceholderList
-          onFillPlaceholder={setPocketPickPendingForPlayer}
-        />
-      );
-    } else if (!hasLabel) {
-      menuContent = (
-        <ActionMenu
-          onProtect={iconCallbacks.onProtect}
-          onStartPocketPick={setPocketPickPendingForPlayer}
-          onVeto={iconCallbacks.onVeto}
-          onRedraw={iconCallbacks.onRedraw}
-          onSetWinner={iconCallbacks.onSetWinner}
-          onCopy={handleCopy}
-        />
-      );
-    } else if (vetoedBy === undefined) {
-      menuContent = (
-        <ActionMenu
-          onSetWinner={iconCallbacks.onSetWinner}
-          onCopy={handleCopy}
-        />
-      );
-    }
-  }
-
   const rootClassname = classNames(styles.chart, {
     [styles.vetoed]: vetoedBy !== undefined,
     [styles.protected]: protectedBy !== undefined,
     [styles.replaced]: replacedBy !== undefined && !baseChartIsPlaceholder,
     [styles.picked]: replacedBy !== undefined && baseChartIsPlaceholder,
-    [styles.clickable]: !!menuContent || !!props.onClick || canCopy,
     [styles.hideVeto]: hideVetos,
     [styles.randomSelected]: wasRandomlySelected,
   });
 
-  const handleCardClick = menuContent ? showMenu : props.onClick || handleCopy;
-
   return (
-    <Popover
-      isOpen={wasRandomlySelected}
-      onClose={clearRandomSelection}
-      content={<div style={{ padding: "0.5em" }}>This one!</div>}
-      targetTagName="div"
-      className={styles.popoverWrapper}
-    >
-      <div
-        ref={rootRef}
-        className={rootClassname}
-        onClick={
-          showingContextMenu || pocketPickPendingForPlayer !== null
-            ? undefined
-            : handleCardClick
-        }
-        style={jacketBg}
-      >
-        <SongSearch
-          isOpen={pocketPickPendingForPlayer !== null}
-          onSongSelect={(song, chart) => {
-            if (actionsEnabled && chart) {
-              iconCallbacks.onReplace(pocketPickPendingForPlayer!, chart);
-            }
-            setPocketPickPendingForPlayer(null);
-          }}
-          onCancel={() => setPocketPickPendingForPlayer(null)}
-        />
+    <div className={styles.cardSlot}>
+      <div ref={rootRef} className={rootClassname} style={jacketBg}>
         <div className={styles.clickTarget}>
           <div className={styles.cardHeader}>
             <img
@@ -175,20 +83,17 @@ export function PersonaSongCard(props: Props) {
           <div className={styles.cardCenter}>
             <CenterContent chart={replacedWith || chart} />
           </div>
-
-          <Popover
-            content={menuContent}
-            isOpen={showingContextMenu}
-            onClose={hideMenu}
-            placement="top"
-            modifiers={{
-              offset: { options: { offset: [0, 35] } },
-            }}
-          >
-            <FooterContent chart={replacedWith || chart} winner={winner} />
-          </Popover>
+          <FooterContent chart={replacedWith || chart} winner={winner} />
         </div>
       </div>
-    </Popover>
+      <div className={styles.coffinCorner}>
+        <img src={coffinBlueIcon} alt="" />
+        <div className={styles.coffinCornerTextBox}>
+          <span className={styles.coffinCornerText}>
+            {bpm}bpm
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
