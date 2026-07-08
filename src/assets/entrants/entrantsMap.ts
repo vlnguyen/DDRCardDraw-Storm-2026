a309bdd4ce4df979663a75bbf487f4ee6e4dcd68
Vincent <vincentlocnguyen@gmail.com>
2026-07-08 10:12:08 -0700

    define eventsMap, alphebetize event keys

--- src/assets/entrants/entrantsMap.ts
+++ src/assets/entrants/entrantsMap.ts
@@ -1,28 +1,180 @@
 export type EventKey =
-  | "ceo2023" // https://www.start.gg/tournament/ceo-2023-3/event/in-the-groove-2-sm5/brackets/1399203/2122916
-  | "ceo2024" // https://www.start.gg/tournament/ceo-2024-6/events/in-the-groove-2-sm5-1/brackets/1583769/2372293/standings
-  | "ceo2025" // https://www.start.gg/tournament/ceo-2025-6/event/itgmania/brackets/1999140/2930433
-  | "rip135" // https://www.start.gg/tournament/rumble-in-the-prairie-13-5/event/itg-tech-singles/standings
-  | "rip14" // https://www.start.gg/tournament/rumble-in-the-prairie-14/event/itg-tech-singles/standings
-  | "rip15" // https://www.start.gg/tournament/rumble-in-the-prairie-15/event/itg-tech-singles/standings
-  | "panini2023" // https://ddrcommunity.com/panini-fest-2023-tournament-results/
-  | "panini2024" // https://challonge.com/paninifest2024_itgtop16/standings
-  | "panini2025" // https://challonge.com/paninifest2025_itg_top16
-  | "itl2023" // https://itl2023.groovestats.com/finalRankings
-  | "itl2024" // https://itl2024.groovestats.com/finalRankings
-  | "itl2025" // https://itl2025.groovestats.com/finalRankings
-  | "itl2026" // https://itl2026.groovestats.com/finalRankings
-  | "bhop1" // https://www.start.gg/tournament/bhop-ball/event/itg-tech-bracket/standings
-  | "bhop2" // https://www.start.gg/tournament/bhop-ball-2/event/itg-tech-bracket/standings
-  | "bhop3" // https://www.start.gg/tournament/bhop-ball-3/event/itg-tech-singles/standings
-  | "bhop4" // https://www.start.gg/tournament/bhop-ball-4/event/itg-tech-singles/standings
-  | "dd2025" // https://www.start.gg/tournament/dash-dance/event/itg-tech-singles/
-  | "shine2024" // https://ddrcommunity.com/shine-tournament-results/
-  | "shine2025" // https://ddrcommunity.com/shine-invitational-2025-tournament-results/
-  | "bite6" // https://www.start.gg/tournament/the-beast-in-the-east-6/event/itg-customs/brackets/1448507/2194521
-  | "bite7" // https://www.start.gg/tournament/beast-in-the-east-7/event/in-the-groove-stepmania/brackets/1729799/2569227
-  | "bite8" // https://www.start.gg/tournament/beast-in-the-east-8/event/itgmania-main-event/brackets/2054340/3006840
-  | "wg2024"; // https://www.start.gg/tournament/winter-groove-2024-itg-tournament/event/itg-tech-singles/brackets/1553467/2332620
+  | "bhop1"
+  | "bhop2"
+  | "bhop3"
+  | "bhop4"
+  | "bite6"
+  | "bite7"
+  | "bite8"
+  | "ceo2023"
+  | "ceo2024"
+  | "ceo2025"
+  | "dd2025"
+  | "itl2023"
+  | "itl2024"
+  | "itl2025"
+  | "itl2026"
+  | "panini2023"
+  | "panini2024"
+  | "panini2025"
+  | "rip135"
+  | "rip14"
+  | "rip15"
+  | "shine2024"
+  | "shine2025"
+  | "wg2024";
+
+export const eventsMap: Record<EventKey, {
+  name: string;
+  url: string;
+  date: string;
+  totalEntrants: number;
+}> = {
+  itl2023: {
+    name: "ITL Online 2023",
+    url: "https://itl2023.groovestats.com/finalRankings",
+    date: "2023-06-19",
+    totalEntrants: 1096,
+  },
+  ceo2023: {
+    name: "CEO 2023",
+    url: "https://www.start.gg/tournament/ceo-2023-3/event/in-the-groove-2-sm5/brackets/1399203/2122916",
+    date: "2023-06-23",
+    totalEntrants: 54,
+  },
+  bite6: {
+    name: "Beast in the East 6",
+    url: "https://www.start.gg/tournament/the-beast-in-the-east-6/event/itg-customs/brackets/1448507/2194521",
+    date: "2023-08-25",
+    totalEntrants: 36,
+  },
+  panini2023: {
+    name: "Panini Fest 2023",
+    url: "https://ddrcommunity.com/panini-fest-2023-tournament-results/",
+    date: "2023-09-02",
+    totalEntrants: 35,
+  },
+  rip135: {
+    name: "Rumble in the Prairie 13.5",
+    url: "https://www.start.gg/tournament/rumble-in-the-prairie-13-5/event/itg-tech-singles/standings",
+    date: "2023-11-17",
+    totalEntrants: 75,
+  },
+  bhop1: {
+    name: "BHOP Ball",
+    url: "https://www.start.gg/tournament/bhop-ball/event/itg-tech-bracket/standings",
+    date: "2023-12-15",
+    totalEntrants: 18,
+  },
+  wg2024: {
+    name: "Winter Groove 2024",
+    url: "https://www.start.gg/tournament/winter-groove-2024-itg-tournament/event/itg-tech-singles/brackets/1553467/2332620",
+    date: "2024-01-12",
+    totalEntrants: 46,
+  },
+  rip14: {
+    name: "Rumble in the Prairie 14",
+    url: "https://www.start.gg/tournament/rumble-in-the-prairie-14/event/itg-tech-singles/standings",
+    date: "2024-03-15",
+    totalEntrants: 84,
+  },
+  itl2024: {
+    name: "ITL Online 2024",
+    url: "https://itl2024.groovestats.com/finalRankings",
+    date: "2024-06-13",
+    totalEntrants: 1394,
+  },
+  ceo2024: {
+    name: "CEO 2024",
+    url: "https://www.start.gg/tournament/ceo-2024-6/events/in-the-groove-2-sm5-1/brackets/1583769/2372293/standings",
+    date: "2024-06-28",
+    totalEntrants: 51,
+  },
+  bhop2: {
+    name: "BHOP Ball 2",
+    url: "https://www.start.gg/tournament/bhop-ball-2/event/itg-tech-bracket/standings",
+    date: "2024-07-12",
+    totalEntrants: 32,
+  },
+  bite7: {
+    name: "Beast in the East 7",
+    url: "https://www.start.gg/tournament/beast-in-the-east-7/event/in-the-groove-stepmania/brackets/1729799/2569227",
+    date: "2024-08-09",
+    totalEntrants: 60,
+  },
+  shine2024: {
+    name: "SHINE",
+    url: "https://ddrcommunity.com/shine-tournament-results/",
+    date: "2024-08-24",
+    totalEntrants: 16,
+  },
+  panini2024: {
+    name: "Panini Fest 2024",
+    url: "https://challonge.com/paninifest2024_itgtop16/standings",
+    date: "2024-09-01",
+    totalEntrants: 32,
+  },
+  bhop3: {
+    name: "BHOP Ball 3",
+    url: "https://www.start.gg/tournament/bhop-ball-3/event/itg-tech-singles/standings",
+    date: "2024-12-06",
+    totalEntrants: 44,
+  },
+  bhop4: {
+    name: "BHOP Ball 4",
+    url: "https://www.start.gg/tournament/bhop-ball-4/event/itg-tech-singles/standings",
+    date: "2025-04-11",
+    totalEntrants: 50,
+  },
+  ceo2025: {
+    name: "CEO 2025",
+    url: "https://www.start.gg/tournament/ceo-2025-6/event/itgmania/brackets/1999140/2930433",
+    date: "2025-06-13",
+    totalEntrants: 71,
+  },
+  itl2025: {
+    name: "ITL Online 2025",
+    url: "https://itl2025.groovestats.com/finalRankings",
+    date: "2025-06-21",
+    totalEntrants: 1703,
+  },
+  bite8: {
+    name: "Beast in the East 8",
+    url: "https://www.start.gg/tournament/beast-in-the-east-8/event/itgmania-main-event/brackets/2054340/3006840",
+    date: "2025-08-22",
+    totalEntrants: 44,
+  },
+  panini2025: {
+    name: "Panini Fest 2025",
+    url: "https://challonge.com/paninifest2025_itg_top16",
+    date: "2025-09-01",
+    totalEntrants: 40,
+  },
+  shine2025: {
+    name: "SHINE Invitational 2025",
+    url: "https://ddrcommunity.com/shine-invitational-2025-tournament-results/",
+    date: "2025-10-11",
+    totalEntrants: 16,
+  },
+  dd2025: {
+    name: "Dash Dance",
+    url: "https://www.start.gg/tournament/dash-dance/event/itg-tech-singles/",
+    date: "2025-11-07",
+    totalEntrants: 46,
+  },
+  rip15: {
+    name: "Rumble in the Prairie 15",
+    url: "https://www.start.gg/tournament/rumble-in-the-prairie-15/event/itg-tech-singles/standings",
+    date: "2026-03-13",
+    totalEntrants: 96,
+  },
+  itl2026: {
+    name: "ITL Online 2026",
+    url: "https://itl2026.groovestats.com/finalRankings",
+    date: "2026-06-25",
+    totalEntrants: 1769,
+  },
+}
 
 export const entrantsMap: Record<number, {
   id: number;
@@ -48,12 +200,12 @@ export const entrantsMap: Record<number, {
     discriminator: "e9b9f217",
     membersId: 173716,
     entrantPlacements: {
-      rip15: 17,
-      panini2025: 13,
       itl2023: 183,
       itl2024: 214,
       itl2025: 110,
       itl2026: 80,
+      panini2025: 13,
+      rip15: 17,
     },
   },
   // BigYama
@@ -74,12 +226,12 @@ export const entrantsMap: Record<number, {
     discriminator: "bdbeaaf0",
     membersId: 61697,
     entrantPlacements: {
-      rip135: 29,
-      panini2024: 13,
       itl2023: 143,
       itl2024: 157,
       itl2025: 184,
       itl2026: 191,
+      panini2024: 13,
+      rip135: 29,
     },
   },
   // Bostic300
@@ -111,17 +263,17 @@ export const entrantsMap: Record<number, {
     discriminator: "db47f574",
     membersId: 128275,
     entrantPlacements: {
-      rip135: 21,
-      rip14: 17,
-      rip15: 37,
-      itl2023: 153,
-      itl2024: 398,
-      itl2025: 1558,
-      itl2026: 788,
       bhop2: 7,
       bhop3: 13,
       bhop4: 13,
       dd2025: 13,
+      itl2023: 153,
+      itl2024: 398,
+      itl2025: 1558,
+      itl2026: 788,
+      rip135: 21,
+      rip14: 17,
+      rip15: 37,
     },
   },
   // Chance R.
@@ -130,19 +282,19 @@ export const entrantsMap: Record<number, {
     discriminator: "3a372353",
     membersId: 133006,
     entrantPlacements: {
+      bhop4: 1,
       ceo2024: 5,
       ceo2025: 1,
-      rip135: 3,
-      rip14: 5,
-      rip15: 1,
-      panini2023: 3,
-      panini2024: 1,
-      panini2025: 1,
       itl2023: 5,
       itl2024: 2,
       itl2025: 1,
       itl2026: 2,
-      bhop4: 1,
+      panini2023: 3,
+      panini2024: 1,
+      panini2025: 1,
+      rip135: 3,
+      rip14: 5,
+      rip15: 1,
       shine2024: 2,
       shine2025: 3,
       wg2024: 5,
@@ -173,12 +325,12 @@ export const entrantsMap: Record<number, {
     discriminator: "04153250",
     membersId: 66673,
     entrantPlacements: {
-      rip14: 13,
-      rip15: 25,
       itl2023: 66,
       itl2024: 75,
       itl2025: 83,
       itl2026: 109,
+      rip14: 13,
+      rip15: 25,
     },
   },
   // cousinoer5
@@ -210,14 +362,14 @@ export const entrantsMap: Record<number, {
     discriminator: "d9a6772f",
     membersId: 147676,
     entrantPlacements: {
-      rip15: 45,
+      bhop3: 17,
+      bhop4: 13,
+      dd2025: 13,
       itl2023: 144,
       itl2024: 187,
       itl2025: 144,
       itl2026: 172,
-      bhop3: 17,
-      bhop4: 13,
-      dd2025: 13,
+      rip15: 45,
     },
   },
   // EvilDave219 [TBD]
@@ -256,15 +408,15 @@ export const entrantsMap: Record<number, {
     discriminator: "baf87bf4",
     membersId: 4306,
     entrantPlacements: {
+      bite8: 4,
       ceo2023: 5,
       ceo2024: 5,
       ceo2025: 7,
-      panini2025: 7,
       itl2023: 18,
       itl2024: 19,
       itl2025: 24,
       itl2026: 40,
-      bite8: 4,
+      panini2025: 7,
       wg2024: 2,
     },
   },
@@ -294,17 +446,17 @@ export const entrantsMap: Record<number, {
     discriminator: "5392e440",
     membersId: 178749,
     entrantPlacements: {
-      rip135: 45,
-      rip14: 21,
-      rip15: 17,
-      itl2024: 122,
-      itl2025: 39,
-      itl2026: 16,
       bhop1: 5,
       bhop2: 5,
       bhop3: 9,
       bhop4: 5,
       dd2025: 7,
+      itl2024: 122,
+      itl2025: 39,
+      itl2026: 16,
+      rip135: 45,
+      rip14: 21,
+      rip15: 17,
     },
   },
   // HeavyMode
@@ -313,12 +465,12 @@ export const entrantsMap: Record<number, {
     discriminator: "3d399b44",
     membersId: 175406,
     entrantPlacements: {
-      rip15: 29,
+      dd2025: 17,
       itl2023: 411,
       itl2024: 194,
       itl2025: 143,
       itl2026: 134,
-      dd2025: 17,
+      rip15: 29,
     },
   },
   // Higgy
@@ -327,11 +479,11 @@ export const entrantsMap: Record<number, {
     discriminator: "8f9d2f17",
     membersId: 171721,
     entrantPlacements: {
-      rip135: 13,
       itl2023: 53,
       itl2024: 78,
       itl2025: 61,
       itl2026: 72,
+      rip135: 13,
     },
   },
   // idontevenknowyou
@@ -385,10 +537,10 @@ export const entrantsMap: Record<number, {
     discriminator: "d1207fcb",
     membersId: 177112,
     entrantPlacements: {
-      rip15: 57,
       itl2024: 395,
       itl2025: 274,
       itl2026: 256,
+      rip15: 57,
     },
   },
   // Lazor
@@ -440,21 +592,21 @@ export const entrantsMap: Record<number, {
     discriminator: "1d674757",
     membersId: 35701,
     entrantPlacements: {
+      bhop2: 3,
+      bhop3: 3,
+      bhop4: 4,
+      bite8: 5,
       ceo2023: 9,
       ceo2024: 7,
       ceo2025: 9,
-      rip14: 5,
-      rip15: 13,
-      panini2025: 5,
+      dd2025: 5,
       itl2023: 15,
       itl2024: 14,
       itl2025: 28,
       itl2026: 10,
-      bhop2: 3,
-      bhop3: 3,
-      bhop4: 4,
-      dd2025: 5,
-      bite8: 5,
+      panini2025: 5,
+      rip14: 5,
+      rip15: 13,
     },
   },
   // mdx
@@ -463,11 +615,11 @@ export const entrantsMap: Record<number, {
     discriminator: "76f9d3bd",
     membersId: 174515,
     entrantPlacements: {
-      rip15: 17,
-      panini2025: 9,
       itl2024: 65,
       itl2025: 241,
       itl2026: 43,
+      panini2025: 9,
+      rip15: 17,
     },
   },
   // meowbois
@@ -486,16 +638,16 @@ export const entrantsMap: Record<number, {
     discriminator: "649127bf",
     membersId: 175365,
     entrantPlacements: {
-      rip135: 17,
-      rip14: 17,
-      rip15: 5,
+      bhop2: 4,
+      bhop3: 7,
+      dd2025: 5,
       itl2023: 117,
       itl2024: 72,
       itl2025: 27,
       itl2026: 17,
-      bhop2: 4,
-      bhop3: 7,
-      dd2025: 5,
+      rip135: 17,
+      rip14: 17,
+      rip15: 5,
       shine2025: 7,
     },
   },
@@ -551,25 +703,25 @@ export const entrantsMap: Record<number, {
     discriminator: "f63b99dd",
     membersId: 4062,
     entrantPlacements: {
-      ceo2023: 1,
-      ceo2024: 2,
-      ceo2025: 4,
-      rip135: 1,
-      rip14: 1,
-      rip15: 2,
-      panini2025: 2,
-      itl2023: 6,
-      itl2024: 6,
-      itl2025: 5,
-      itl2026: 5,
       bhop1: 1,
       bhop2: 2,
       bhop3: 2,
       bhop4: 2,
+      bite8: 1,
+      ceo2023: 1,
+      ceo2024: 2,
+      ceo2025: 4,
       dd2025: 1,
+      itl2023: 6,
+      itl2024: 6,
+      itl2025: 5,
+      itl2026: 5,
+      panini2025: 2,
+      rip135: 1,
+      rip14: 1,
+      rip15: 2,
       shine2024: 6,
       shine2025: 1,
-      bite8: 1,
     },
   },
   // Ryuguu
@@ -587,11 +739,11 @@ export const entrantsMap: Record<number, {
     discriminator: "1ec6593b",
     membersId: 173554,
     entrantPlacements: {
-      rip135: 33,
       itl2023: 308,
       itl2024: 477,
       itl2025: 656,
       itl2026: 780,
+      rip135: 33,
     },
   },
   // simplySOUF [STUUF]
@@ -617,17 +769,17 @@ export const entrantsMap: Record<number, {
     discriminator: "3c532367",
     membersId: 178992,
     entrantPlacements: {
-      rip135: 61,
-      rip14: 73,
-      rip15: 61,
-      itl2024: 1149,
-      itl2025: 550,
-      itl2026: 463,
       bhop1: 13,
       bhop2: 27,
       bhop3: 33,
       bhop4: 29,
       dd2025: 33,
+      itl2024: 1149,
+      itl2025: 550,
+      itl2026: 463,
+      rip135: 61,
+      rip14: 73,
+      rip15: 61,
     },
   },
   // SpeakEZ
@@ -636,9 +788,9 @@ export const entrantsMap: Record<number, {
     discriminator: "03229ef1",
     membersId: 188881,
     entrantPlacements: {
-      rip15: 69,
       itl2025: 1041,
       itl2026: 745,
+      rip15: 69,
     },
   },
   // Sudzi
@@ -647,14 +799,14 @@ export const entrantsMap: Record<number, {
     discriminator: "a24fd984",
     membersId: 36049,
     entrantPlacements: {
-      rip135: 5,
-      rip14: 7,
-      rip15: 9,
-      panini2025: 9,
       itl2023: 44,
       itl2024: 44,
       itl2025: 32,
       itl2026: 20,
+      panini2025: 9,
+      rip135: 5,
+      rip14: 7,
+      rip15: 9,
       shine2025: 4,
     },
   },
@@ -664,12 +816,12 @@ export const entrantsMap: Record<number, {
     discriminator: "2ddd55d0",
     membersId: 8349,
     entrantPlacements: {
-      rip14: 13,
-      rip15: 37,
       itl2023: 61,
       itl2024: 1398,
       itl2025: 85,
       itl2026: 1689,
+      rip14: 13,
+      rip15: 37,
     },
   },
   // teejusb
@@ -678,12 +830,12 @@ export const entrantsMap: Record<number, {
     discriminator: "ea646ec3",
     membersId: 50287,
     entrantPlacements: {
-      rip135: 25,
-      rip15: 45,
       itl2023: 120,
       itl2024: 139,
       itl2025: 236,
       itl2026: 277,
+      rip135: 25,
+      rip15: 45,
     },
   },
   // TommyDoesntMiss
@@ -692,14 +844,14 @@ export const entrantsMap: Record<number, {
     discriminator: "75cf6b41",
     membersId: 66784,
     entrantPlacements: {
-      rip135: 9,
-      rip15: 21,
+      bhop3: 13,
+      dd2025: 9,
       itl2023: 67,
       itl2024: 87,
       itl2025: 76,
       itl2026: 92,
-      bhop3: 13,
-      dd2025: 9,
+      rip135: 9,
+      rip15: 21,
     },
   },
   // Ty
@@ -733,16 +885,16 @@ export const entrantsMap: Record<number, {
     discriminator: "226cd505",
     membersId: 65671,
     entrantPlacements: {
+      bhop3: 5,
       ceo2025: 9,
-      rip135: 9,
-      rip14: 9,
-      rip15: 21,
+      dd2025: 7,
       itl2023: 29,
       itl2024: 17,
       itl2025: 30,
       itl2026: 68,
-      bhop3: 5,
-      dd2025: 7,
+      rip135: 9,
+      rip14: 9,
+      rip15: 21,
     },
   },
   // VivaLaMoo [STORM]
@@ -751,22 +903,22 @@ export const entrantsMap: Record<number, {
     discriminator: "3aadda80",
     membersId: 4362,
     entrantPlacements: {
+      bhop3: 1,
+      bhop4: 5,
+      bite7: 3,
+      bite8: 2,
       ceo2023: 3,
       ceo2025: 5,
-      rip14: 2,
-      rip15: 9,
-      panini2023: 1,
-      panini2025: 4,
+      dd2025: 2,
       itl2023: 8,
       itl2024: 10,
       itl2025: 12,
       itl2026: 7,
-      bhop3: 1,
-      bhop4: 5,
-      dd2025: 2,
+      panini2023: 1,
+      panini2025: 4,
+      rip14: 2,
+      rip15: 9,
       shine2024: 3,
-      bite7: 3,
-      bite8: 2,
       wg2024: 4,
     },
   },
@@ -776,8 +928,8 @@ export const entrantsMap: Record<number, {
     discriminator: "1f8376b0",
     membersId: 180548,
     entrantPlacements: {
-      itl2024: 487,
       bhop3: 13,
+      itl2024: 487,
     },
   },
   // X!!
@@ -799,14 +951,14 @@ export const entrantsMap: Record<number, {
     membersId: 173043,
     entrantPlacements: {
       ceo2025: 9,
-      rip14: 9,
-      rip15: 21,
-      panini2024: 5,
-      panini2025: 9,
       itl2023: 77,
       itl2024: 42,
       itl2025: 62,
       itl2026: 51,
+      panini2024: 5,
+      panini2025: 9,
+      rip14: 9,
+      rip15: 21,
     },
   },
   // Yokamaa [MOISTBOIS]
@@ -815,12 +967,12 @@ export const entrantsMap: Record<number, {
     discriminator: "1ee0ca7e",
     membersId: 170806,
     entrantPlacements: {
-      panini2024: 7,
-      panini2025: 13,
       itl2023: 237,
       itl2024: 83,
       itl2025: 137,
       itl2026: 107,
+      panini2024: 7,
+      panini2025: 13,
     },
   },
   // Yung Buttcoin
